5c5423d934bd47065b04f204e727546758e4e50e
Scott Conway <scott.conway@gmail.com>
2019-09-22 20:49:43 -0500

    Added missing javadoc.
    About two-thirds finished with the validation javadoc.

--- src/main/java/com/opencsv/bean/validators/MustMatchRegexExpression.java
+++ src/main/java/com/opencsv/bean/validators/MustMatchRegexExpression.java
@@ -6,7 +6,25 @@ import com.opencsv.exceptions.CsvValidationException;
 import java.util.ResourceBundle;
 
 /**
- * TODO: Javadoc
+ * <p>This is a validator that due to the addition of the parameter allows the validation of multiple different types
+ * of input.  The paramString must be a valid regular expression.  The MustMatchRegularExpression validator will
+ * do call the String matches method of the string to be converted and the regular expression string and if
+ * the two do not match then a {@link CsvValidationException} will be thrown.</p>
+ * <p>Because this is validating the string BEFORE it is parsed/converted the capture settings
+ * of the string must be taken into account.</p>
+ * <p>Examples:</p>
+ * <pre>
+ *     // The String that becomes id must be a number with three to six digits.
+ *     &#64;PreAssignmentValidator(validator = MustMatchRegexExpression.class, paramString = "^[0-9]{3,6}$")
+ *     &#64;CsvBindByName(column = "id")
+ *     private int beanId;
+ *
+ *     // The String that becomes bigNumber must be a number with seven to ten digits.
+ *     // The String for this field is after the word "value: " in the field.
+ *     &#64;PreAssignmentValidator(validator = MustMatchRegexExpression.class, paramString = "^[A-Za-z ]*value: [0-9]{7,10}$")
+ *     &#64;CsvBindByName(column = "big number", capture = "^[A-Za-z ]*value: (.*)$", format = "value: %s")
+ *     private long bigNumber;
+ * </pre>
  */
 public class MustMatchRegexExpression implements StringValidator {
     private String regex = "";
