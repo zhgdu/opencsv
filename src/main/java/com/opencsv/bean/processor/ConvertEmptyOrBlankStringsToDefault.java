b9872baa2e7c935560ce847b4cdf25d1b945aab7
Scott Conway <scott.conway@gmail.com>
2020-11-20 14:46:36 -0600

    Moved some of the StringProcessors I created for unit and integration tests out of the test package and into src under com.opencsv.bean.processors.

---
+++ src/main/java/com/opencsv/bean/processor/ConvertEmptyOrBlankStringsToDefault.java
@@ -0,0 +1,33 @@
+package com.opencsv.bean.processor;
+
+/**
+ * StringProcessor that converts the empty or blank strings to a desired value string.
+ * This is useful when you want a default value.
+ * <p>
+ * A sample of this can be found in the unit test ProcessorTestBean and is annotated as follows.
+ *
+ * <pre>
+ *     @PreAssignmentProcessor(processor = ConvertEmptyOrBlankStringsToDefault.class, paramString = "-1")
+ *     @CsvBindByName(column = "id")
+ *     private int beanId;
+ *  </pre>
+ *
+ * @author Scott Conway
+ * @since 5.4
+ */
+public class ConvertEmptyOrBlankStringsToDefault implements StringProcessor {
+    String defaultValue;
+
+    @Override
+    public String processString(String value) {
+        if (value == null || value.trim().isEmpty()) {
+            return defaultValue;
+        }
+        return value;
+    }
+
+    @Override
+    public void setParameterString(String value) {
+        defaultValue = value;
+    }
+}
---
+++ src/main/java/com/opencsv/bean/processor/ConvertEmptyOrBlankStringsToNull.java
@@ -0,0 +1,37 @@
+package com.opencsv.bean.processor;
+
+/**
+ * StringProcessor that converts the empty or blank strings to a literal null string.
+ * This is useful when you prefer null in a particular variable.
+ * <p>
+ * A sample of this can be found in the unit test ProcessorTestBean and is annotated as follows.
+ *
+ * <pre>
+ *     @PreAssignmentProcessor(processor = ConvertEmptyOrBlankStringsToNull.class)
+ *     @CsvBindByName(column = "name")
+ *     private String beanName;
+ *  </pre>
+ *
+ * @author Scott Conway
+ * @since 5.4
+ */
+public class ConvertEmptyOrBlankStringsToNull implements StringProcessor {
+    @Override
+    public String processString(String value) {
+        if (value == null || value.trim().isEmpty()) {
+            return null;
+        }
+        return value;
+    }
+
+    /**
+     * This method is unused in this implementation as we are converting to null.
+     * Any calls to this method are ignored.
+     *
+     * @param value Information used by the processor to process the string
+     */
+    @Override
+    public void setParameterString(String value) {
+        // not needed
+    }
+}
